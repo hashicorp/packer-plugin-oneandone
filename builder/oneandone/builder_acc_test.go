6d5c3fc044be99c7612fc3ea2196e62bb993dfd7
sylviamoss <moss@hashicorp.com>
2021-05-25 14:07:43 +0200

    Remove uncessary version and rewrite acc tests

--- builder/oneandone/builder_acc_test.go
+++ builder/oneandone/builder_acc_test.go
@@ -1,24 +1,34 @@
 package oneandone
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
 	"testing"
 
-	builderT "github.com/hashicorp/packer/acctest"
+	"github.com/hashicorp/packer-plugin-sdk/acctest"
 )
 
 func TestBuilderAcc_basic(t *testing.T) {
-	builderT.Test(t, builderT.TestCase{
-		PreCheck: func() { testAccPreCheck(t) },
-		Builder:  &Builder{},
+	testCase := &acctest.PluginTestCase{
+		Name:     "oneandone_basic_test",
 		Template: testBuilderAccBasic,
-	})
-}
-
-func testAccPreCheck(t *testing.T) {
-	if v := os.Getenv("ONEANDONE_TOKEN"); v == "" {
-		t.Fatal("ONEANDONE_TOKEN must be set for acceptance tests")
+		Setup: func() error {
+			if v := os.Getenv("ONEANDONE_TOKEN"); v == "" {
+				t.Fatal("ONEANDONE_TOKEN must be set for acceptance tests")
+			}
+			return nil
+		},
+		Check: func(buildCommand *exec.Cmd, logfile string) error {
+			if buildCommand.ProcessState != nil {
+				if buildCommand.ProcessState.ExitCode() != 0 {
+					return fmt.Errorf("Bad exit code. Logfile: %s", logfile)
+				}
+			}
+			return nil
+		},
 	}
+	acctest.TestPlugin(t, testCase)
 }
 
 const testBuilderAccBasic = `
